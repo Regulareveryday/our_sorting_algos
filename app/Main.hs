25c3c49008951685b1f564133a557a73d355e358
Regulareveryday <julia.sypien@gmail.com>
2017-01-29 12:41:02 +0100

    main

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,4 @@
-{-|
-Module      : Main
-Description : main module of our sorting algos project
-Copyright   : (c) Julia Sypień, Patrycja Tarasińska, 2017
-License     : GPL-3
-Maintainer  : sasuke96@tlen.pl
-Stability   : experimental
-Portability : POSIX
--}
+
 module Main where
 
 import Heapsort
@@ -17,14 +9,13 @@ import Selectionsort
 import Bubblesort
 
 
--- * functions
--- |main function that gives some iinformation about our project
+
 main :: IO ()
 main = do
   putStr "\nWelcome to our project: time comparision of sorting algorithms in Haskell! Explore different sorting algos!\n\n"
   putStr "\nChoose 'options' to see what algorithms you can use\n"
 
--- |function that helps users explore the program
+  
 options :: IO ()
 options = do
      putStr "\nbubblesort, insertionsort, heapsort, mergesort, selectionsort, quicksort\n"
